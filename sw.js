9a08bd23662317911198054f483aee66077a736f
comstocd <65256409+comstocd@users.noreply.github.com>
2026-04-13 18:18:59 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@
  */
 
 // ── CHANGE THESE TWO LINES EVERY TIME YOU PUSH A CLINICAL UPDATE ─────────────
-const DISPLAY_VERSION = 'BUILD #53.3';           // shown everywhere in the app
-const CACHE_VERSION   = 'crs-v6.5-build53.3';   // must be unique per release
+const DISPLAY_VERSION = 'BUILD #53.4';           // shown everywhere in the app
+const CACHE_VERSION   = 'crs-v6.5-build53.4';   // must be unique per release
 // ─────────────────────────────────────────────────────────────────────────────
 
 const CACHE_NAME = `clinical-ref-${CACHE_VERSION}`;
